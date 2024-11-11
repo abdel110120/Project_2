7bd6bdd86510a5f2cb4b184f7b793c91a16ac81d
Abdelilah NAJIHI <abdelilahnajihi@Abdelilahs-MacBook-Air.local>
2024-11-11 22:15:35 +0100

    Add routing

---
+++ src/app/my-c/my-c.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-c',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-c.component.html',
+  styleUrl: './my-c.component.css'
+})
+export class MyCComponent {
+
+}
